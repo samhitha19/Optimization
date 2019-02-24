2ad65badda109e3ec45d1bf333dc593cbfcb35b9
samhitha19 <42738874+samhitha19@users.noreply.github.com>
2019-02-24 11:13:13 +0530

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,14 @@
+from cvxopt import matrix
+from cvxopt import solvers
+
+c = matrix([1.,1.,0.])
+G = [ matrix([[-1., 0., 0., 0.],[ 0., -1., -1., 0.],[0.,  0.,  0., -1.]]) ]
+
+Aval = matrix([1.,0.,1.],(1,3))
+bval = matrix([1.])
+
+h = [ matrix([[0., 0.], [0., 0.]]) ]
+sol = solvers.sdp(c, Gs=G, hs=h,A=Aval, b=bval)
+print(sol['x'])
+print(sol['x'][0]+sol['x'][1])
+print('found at' ,sol['x'][0] ,'and' ,sol['x'][1])
